fe554dd358522b9738248c8ef4ce098122bccb49
jstefan14 <36207732+jstefan14@users.noreply.github.com>
2018-03-26 18:29:17 -0400

    Updated model to reflect Tree DB on excel sheet

--- wfu_tree_hogan/models/usersModel.js
+++ wfu_tree_hogan/models/usersModel.js
@@ -3,9 +3,9 @@
 
 var users_data = 
 [
-  	{name: 'Frodo', lastname: 'Baggins'}, 
-  	{name: 'Sam', lastname: 'Gamgee'},
-  	{name: 'Peregrin', lastname: 'Took'} 
+  	{comName: 'Willow Oak', sciName: 'Quercus phellos', longitude: '-80.27982421180033', latitude:'36.1352867683009', family:'Fagaceae', order:'Fagales', type:'Oak' }, 
+  	{comName: 'Eastern Hophornbeam', sciName: 'Ostrya virginiana', longitude: '-80.27945222471334', latitude:'36.13399855678345', family:'Tulaceae', order:'', type:'Ironwood'},
+  	{comName: 'Willow Oak', sciName: 'Quercus phellos', longitude: '80.27982421180033', latitude:'36.1352867683009', family:'Fagaceae', order:'Fagales', type:'Oak'} 
 ];
 
 module.exports = users_data;
